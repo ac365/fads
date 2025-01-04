fc18afade05a571a1dede22810aeea2e3f6c8fe0
Apoorva Chaubal <chaubal.4@osu.edu>
2025-01-03 18:26:28 -0700

    fixed aero model

--- ifs/aero/fleemanAeroBuildUp.py
+++ ifs/aero/fleemanAeroBuildUp.py
@@ -10,34 +10,40 @@ class Aero:
         self.noseFineness = noseFineness
         self.bodyWidth    = bodyWidth
         self.bodyHeight   = bodyHeight
-
-    def zeroLiftDrag(self, mach:np.double, qBar:np.double,
-                     powerOn:bool, Ae:np.double):
-        
-        supersonic = mach >= 1.0
-        if supersonic:
-            baseDrag = 0.25/mach*(powerOn*(1-Ae/self.sRef))
-        else:
-            baseDrag = (0.12 + 0.13*mach*mach)*(powerOn*(1-Ae/self.sRef))
-        waveDrag   = ((1.586 + 1.834/(mach*mach))*
-            math.pow(math.atan(0.5/self.noseFineness),1.69))
-        skinDrag = (0.053*(self.bodyLenFt/self.bodyDiamFt)*
-            math.pow(mach/(qBar*self.bodyLenFt)))
-
-        Cd0 = waveDrag + baseDrag + skinDrag
-        return Cd0 
     
-    def computeForces(self, rollAng:np.double, alpha:np.double,
-                     mach:np.double, qBar:np.double, powerOn:bool,
-                     Ae:np.double):
-        Cd0 = self._zeroLiftDrag(mach,qBar,powerOn,Ae)
+    def computeForces(self, phi:np.double, alpha:np.double,
+                      mach:np.double, qBar:np.double, powerOn:bool,
+                      Ae:np.double, rho:np.double, velMag:np.double):
+        #axial
+        if mach >= 1.0:
+            baseDrag = 0.25/mach
+        else:
+            baseDrag = (0.12 + 0.13*mach*mach)
+        if powerOn:
+            baseDrag *= (1-Ae/self.sRef)
 
-        Cn  = self.bodyWidth/self.bodyHeight*math.cos(rollAng)**2
-        Cn += self.bodyHeight/self.bodyWidth*math.sin(rollAng)**2
+        if mach >= 0.8:
+            waveDrag = ((1.586 + 1.834/(mach*mach))*
+                        math.pow(math.atan(0.5/self.noseFineness),1.69))
+        else:
+            waveDrag = 0.0
+        
+        skinDrag = (0.053*(self.bodyLenFt/self.bodyDiamFt)*
+                    math.pow(mach/(qBar*self.bodyLenFt)))
+        
+        Cd0 = waveDrag + baseDrag + skinDrag
+        
+        #normal
+        Cn  = self.bodyWidth/self.bodyHeight*math.cos(phi)**2
+        Cn += self.bodyHeight/self.bodyWidth*math.sin(phi)**2
         Cn *= (abs(math.sin(2*alpha)*math.cos(alpha/2)) + 
                1.3*self.bodyLenFt/self.bodyDiamFt*math.sin(alpha)**2)
         
-        Cl = Cn*math.cos(alpha) - Cd0*math.sin(alpha)
-        Cd = Cn*math.sin(alpha) + Cd0*math.cos(alpha)
+        #body
+        Cy = Cn*math.sin(phi)
+        Cz = Cn*math.cos(phi)
 
-        return Cl,Cd
+        aeroForces  = np.array([-Cd0,Cy,Cz])
+        aeroForces *= 0.5*rho*velMag*velMag
+
+        return aeroForces
