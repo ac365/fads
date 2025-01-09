93df536876cdd667da51f7ea1bcca9ea23b47971
Apoorva Chaubal <chaubal.4@osu.edu>
2025-01-08 23:25:04 -0700

    fixed aero model

--- ifs/aero/fleemanAero.py
+++ ifs/aero/fleemanAero.py
@@ -2,18 +2,18 @@ import numpy as np
 import math
 
 class Aero:
-    def __init__(self, noseFineness, bodyLen, bodyDiam, sRef,
-                 bodyWidth, bodyHeight):
-        self.sRef         = sRef
-        self.bodyLen      = bodyLen
-        self.bodyDiam     = bodyDiam
-        self.noseFineness = noseFineness
-        self.bodyWidth    = bodyWidth
-        self.bodyHeight   = bodyHeight
+    def __init__(self, noseFineness, bodyLen, bodyDiam, bodyWidth, 
+                 bodyHeight):
+        self._bodyLen      = bodyLen
+        self._bodyDiam     = bodyDiam
+        self._noseFineness = noseFineness
+        self._bodyWidth    = bodyWidth
+        self._bodyHeight   = bodyHeight
+        self._sRef         = math.pi/4*bodyWidth*bodyHeight
     
     def computeForces(self, phi:np.double, alpha:np.double,
                       mach:np.double, qBar:np.double, powerOn:bool,
-                      Ae:np.double, rho:np.double, velMag:np.double):
+                      Ae:np.double):
         #axial
         if mach >= 1.0:
             baseDrag = 0.25/mach
@@ -24,27 +24,27 @@ class Aero:
 
         if mach >= 0.8:
             waveDrag = ((1.586 + 1.834/(mach*mach))*
-                        math.pow(math.atan(0.5/self.noseFineness),1.69))
+                        math.pow(math.atan(0.5/self._noseFineness),1.69))
         else:
             waveDrag = 0.0
         
-        skinDrag = (0.053*(self.bodyLen/self.bodyDiam)*
-                    math.pow(mach/(qBar*self.bodyLen)))
+        skinDrag = (0.053*(self._bodyLen/self._bodyDiam)*
+                    math.pow(mach/(qBar*self._bodyLen),0.2))
 
         Cd0 = waveDrag + baseDrag + skinDrag
         
         #normal
-        Cn  = self.bodyWidth/self.bodyHeight*math.cos(phi)**2
-        Cn += self.bodyHeight/self.bodyWidth*math.sin(phi)**2
+        Cn  = self._bodyWidth/self._bodyHeight*math.cos(phi)**2
+        Cn += self._bodyHeight/self._bodyWidth*math.sin(phi)**2
         Cn *= (abs(math.sin(2*alpha)*math.cos(alpha/2)) + 
-               1.3*self.bodyLen/self.bodyDiam*math.sin(alpha)**2)
+               1.3*self._bodyLen/self._bodyDiam*math.sin(alpha)**2)
         
         #body
-        Cx = -Cd0*math.cos(alpha)
+        Cx = -Cn*math.sin(alpha) - Cd0*math.cos(alpha)
         Cy = -Cn*math.cos(phi)
-        Cz = -Cn*math.sin(phi)
+        Cz = -Cn*math.sin(phi) + Cd0*math.sin(alpha)
 
         aeroForces  = np.array([Cx,Cy,Cz])
-        aeroForces *= qBar*self.sRef
+        aeroForces *= qBar*self._sRef
 
         return aeroForces
