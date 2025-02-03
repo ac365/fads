96d65ca1112f8155c5cd8fe53066cf7fd11d9523
Apoorva Chaubal <chaubal.4@osu.edu>
2025-02-02 23:01:06 -0700

    updated nav event with information needed for pseudo-autopilot

--- core/events/navEvent.py
+++ core/events/navEvent.py
@@ -3,14 +3,15 @@ from dataclasses import dataclass
 
 @dataclass
 class navEvent:
-    quat   :np.array
+    quat:np.array
     angRate:np.array
-    angAcc :np.array
-    posNed :np.array
-    velNed :np.array
-    accNed :np.array
-    e2b    :np.matrix
-    mass   :np.double
+    angAcc:np.array
+    posNed:np.array
+    velNed:np.array
+    accNed:np.array
+    e2b:np.matrix
+    mass:np.double
+    bdyForces:np.array
 
     name:str = "nav"
 
