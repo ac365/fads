ed457000accc258efa07bbdb8a571226ae72f276
Apoorva Chaubal <chaubal.4@osu.edu>
2025-02-02 23:11:28 -0700

    made minor name changes to ap params

--- ofs/autopilot/dynInvAutopilotParams.py
+++ ofs/autopilot/dynInvAutopilotParams.py
@@ -2,13 +2,13 @@ import numpy as np
 from dataclasses import dataclass
 
 @dataclass
-class autopilotLateral:
+class apLateral:
     tau:np.double = 0.1
     xi:np.double  = 0.71
     w:np.double   = 16.05
     wz:np.double  = 20.5
 
 @dataclass
-class autopilotRoll:
+class apRoll:
     xi:np.double = 0.71
     w:np.double  = 7
